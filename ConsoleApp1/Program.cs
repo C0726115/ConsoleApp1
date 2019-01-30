c75391d3484c4d107a6c88d92319fc0675f62f2e
visualstudio <visualstudio@122-PC26>
2019-01-30 10:37:41 -0500

    code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace _C0726115_week_04
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
+
+    class Node
+    {
+        class Village
+
+        {
+            // Node is an ADT
+            // what KInd of DATA do we need in a NODE?
+            public Village nextVillage;
+            public String VillageName;
+            public bool isAstrildeHere = false;
+
+        }
+
+    }
 }
+
